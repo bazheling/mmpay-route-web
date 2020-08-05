7f8d6743a0fcafd85ce4bfdebec0340062122dc3
pinky <1446879267@qq.com>
2020-08-05 14:50:09 +0800

    1.将mapper放到resource文件夹下 2.pom文件修订

--- src/main/java/com/ylzinfo/controller/MmpayRouteController.java
+++ src/main/java/com/ylzinfo/controller/MmpayRouteController.java
@@ -2,6 +2,8 @@ package com.ylzinfo.controller;
 
 import com.ylzinfo.dao.entity.GirlsInfo;
 import com.ylzinfo.dao.mappers.GirlsInfoMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
@@ -13,11 +15,16 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/mmpay")
 public class MmpayRouteController {
 
+    private static final Logger LOG = LoggerFactory.getLogger(MmpayRouteController.class);
+
+
     @Autowired
     private GirlsInfoMapper girlsInfoMapper;
     @RequestMapping("getuser")
     @ResponseBody
     public GirlsInfo getUser() {
+        LOG.info("!qqqq");
+
         GirlsInfo girlsInfo = girlsInfoMapper.selectByPrimaryKey(Integer.parseInt("1"));
         return girlsInfo;
     }
