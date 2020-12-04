9b9e85b23d2b15c3e5798cf07ab1bc1f127a6e65
linchunling <1365165054@qq.com>
2020-12-04 15:36:49 +0800

    1.清理不需要得代码段

--- src/main/java/com/ylzinfo/RouteInitializer.java
+++ src/main/java/com/ylzinfo/RouteInitializer.java
@@ -8,9 +8,6 @@ import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.stereotype.Component;
 
-import java.io.InputStream;
-import java.util.Properties;
-
 /**
  * @author lcl
  * @date 2020/12/2
@@ -32,14 +29,6 @@ public class RouteInitializer implements BeanFactoryPostProcessor {
         try {
             // 初始化DdjsProperties常量类
             CommonUtils.loadProperties(MMPAYROUTE_PROPERTIES_FILE, MmpayRouteProperties.class);
-//            Properties properties = new Properties();
-//            // 使用ClassLoader加载properties配置文件生成对应的输入流
-//            InputStream in = MmpayRouteApplication.class.getClassLoader().getResourceAsStream(MMPAYROUTE_PROPERTIES_FILE);
-//            // 使用properties对象加载输入流
-//            properties.load(in);
-//            MmpayRouteProperties.jsonPath=properties.getProperty("jsonPath");
-//            MmpayRouteProperties.getAppWay=properties.getProperty("getAppWay");
-//            获取key对应的value值
             logger.info("mmpay route system init finish...");
         } catch (Exception e) {
             logger.error("mmpay route  init fail ", e);
